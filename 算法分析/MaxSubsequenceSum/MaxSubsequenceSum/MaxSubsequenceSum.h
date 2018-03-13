62bf48864f45b9860f2aaa75ce2095ebe7dbae40
Fang Chao <fc_btby@hotmail.com>
2018-03-13 18:13:55 +0800

    basic algorithm

---
+++ 算法分析/MaxSubsequenceSum/MaxSubsequenceSum/MaxSubsequenceSum.h
@@ -0,0 +1,15 @@
+#pragma once
+#ifndef MaxSubsequenceSum_H
+#define MaxSubsequenceSun_H
+
+#include<stdio.h>
+
+
+int MaxSubsequenceSum1(const int A[], int N);
+
+int MaxSubsequenceSum2(const int A[], int N);
+
+int MaxSubsequenceSum3(const int A[], int N);
+
+int MaxSubsequenceSum4(const int A[], int N);
+#endif // !MaxSubsequenceSum_H
